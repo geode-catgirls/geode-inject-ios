94aee4c537ec9ae9fa311ddc9a7e7588a8aa28b3
rooot <hey@rooot.gay>
2025-03-11 16:58:26 +0100

    chore: format codebase using clang-format
    Signed-off-by: rooot <hey@rooot.gay>

--- src/geode.m
+++ src/geode.m
@@ -1,5 +1,5 @@
-#include <dlfcn.h>
 #include <Foundation/Foundation.h>
+#include <dlfcn.h>
 
 #include <stdlib.h>
 
@@ -15,17 +15,17 @@
 
 // what the actual fuck is this entire function.
 // no like seriously who tf came up with objc and the ios sdk
-void showAlert(NSString *tiddies, NSString *meows, bool remeowbutton) {
+void showAlert(NSString* tiddies, NSString* meows, bool remeowbutton) {
 	dispatch_async(dispatch_get_main_queue(), ^{
-		UIViewController *currentFucker = [[[UIApplication sharedApplication] windows].firstObject rootViewController];
+		UIViewController* currentFucker = [[[UIApplication sharedApplication] windows].firstObject rootViewController];
 
-		UIAlertController *alert = [UIAlertController alertControllerWithTitle:tiddies message:meows preferredStyle:UIAlertControllerStyleAlert];
+		UIAlertController* alert = [UIAlertController alertControllerWithTitle:tiddies message:meows preferredStyle:UIAlertControllerStyleAlert];
 
-		UIAlertAction *fuckoff = [UIAlertAction actionWithTitle:@"go away" style:UIAlertActionStyleDefault handler:nil];
+		UIAlertAction* fuckoff = [UIAlertAction actionWithTitle:@"go away" style:UIAlertActionStyleDefault handler:nil];
 		[alert addAction:fuckoff];
 
 		if (remeowbutton) {
-			UIAlertAction *restart = [UIAlertAction actionWithTitle:@"restart" style:UIAlertActionStyleDefault handler:^(UIAlertAction* _) { exit(0); }];
+			UIAlertAction* restart = [UIAlertAction actionWithTitle:@"restart" style:UIAlertActionStyleDefault handler:^(UIAlertAction* _) { exit(0); }];
 			[alert addAction:restart];
 		}
 
@@ -36,18 +36,18 @@ void showAlert(NSString *tiddies, NSString *meows, bool remeowbutton) {
 void init_loadGeode(void) {
 	NSLog(@"mrow init_loadGeode");
 
-	NSArray *paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES);
-	NSString *applicationSupportDirectory = [paths firstObject];
+	NSArray* paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES);
+	NSString* applicationSupportDirectory = [paths firstObject];
 
-	NSString *geode_dir = [applicationSupportDirectory stringByAppendingString:@"/GeometryDash/game/geode"];
-	NSString *geode_lib = [geode_dir stringByAppendingString:@"/Geode.ios.dylib"];
-	NSString *geode_env = [geode_dir stringByAppendingString:@"/geode.env"];
+	NSString* geode_dir = [applicationSupportDirectory stringByAppendingString:@"/GeometryDash/game/geode"];
+	NSString* geode_lib = [geode_dir stringByAppendingString:@"/Geode.ios.dylib"];
+	NSString* geode_env = [geode_dir stringByAppendingString:@"/geode.env"];
 
 	bool is_dir;
-	NSFileManager *fm = [NSFileManager defaultManager];
-	if(![fm fileExistsAtPath:geode_dir isDirectory:&is_dir]) {
+	NSFileManager* fm = [NSFileManager defaultManager];
+	if (![fm fileExistsAtPath:geode_dir isDirectory:&is_dir]) {
 		NSLog(@"mrow creating geode dir !!");
-		if(![fm createDirectoryAtPath:geode_dir withIntermediateDirectories:YES attributes:nil error:NULL]) {
+		if (![fm createDirectoryAtPath:geode_dir withIntermediateDirectories:YES attributes:nil error:NULL]) {
 			NSLog(@"mrow failed to create folder!!");
 		}
 	}
@@ -60,17 +60,17 @@ void init_loadGeode(void) {
 
 	if (!geode_exists) {
 		NSLog(@"mrow geode dylib DOES NOT EXIST! downloading...");
-		NSURL *url = [NSURL URLWithString:GEODE_DOWNLOAD_URL];
-		NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
-		NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+		NSURL* url = [NSURL URLWithString:GEODE_DOWNLOAD_URL];
+		NSURLSessionConfiguration* configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+		NSURLSession* session = [NSURLSession sessionWithConfiguration:configuration];
 
-		NSURLSessionDownloadTask *downloadTask = [session downloadTaskWithURL:url completionHandler:^(NSURL *location, NSURLResponse *response, NSError *error) {
+		NSURLSessionDownloadTask* downloadTask = [session downloadTaskWithURL:url completionHandler:^(NSURL* location, NSURLResponse* response, NSError* error) {
 			if (error) {
 				showAlert(@"error", @"waaaaaaaa", false);
 				NSLog(@"mrow FAILED to download Geode: %@", error);
 			} else {
-				//NSFileManager *fileManager = [NSFileManager defaultManager];
-				NSError *moveError = nil;
+				// NSFileManager *fileManager = [NSFileManager defaultManager];
+				NSError* moveError = nil;
 				if ([fm moveItemAtURL:location toURL:[NSURL fileURLWithPath:geode_lib] error:&moveError]) {
 					NSLog(@"mrow SUCCESS - downloaded Geode!");
 					showAlert(@"Downloaded Geode", @"Successfully downloaded Geode.ios.dylib\nRestart the game to use Geode.", true);
@@ -96,7 +96,7 @@ void init_loadGeode(void) {
 				}
 
 				NSString* key = parts[0];
-				NSString* val = [[parts subarrayWithRange:NSMakeRange(1, parts.count-1)] componentsJoinedByString:@"="];
+				NSString* val = [[parts subarrayWithRange:NSMakeRange(1, parts.count - 1)] componentsJoinedByString:@"="];
 				if ([val hasPrefix:@"\""] && [val hasSuffix:@"\""]) {
 					val = [[val substringToIndex:[val length] - 1] substringFromIndex:1];
 					NSLog(@"mrow stripped quotes from env val: %@", val);
@@ -120,7 +120,5 @@ void init_loadGeode(void) {
 	dlopen([geode_lib UTF8String], RTLD_LAZY);
 
 	NSLog(@"mrow inhibiting screen sleep (in 1s)");
-	[NSTimer scheduledTimerWithTimeInterval:1.0 repeats:NO block:^(NSTimer* meow) {
-		[UIApplication sharedApplication].idleTimerDisabled = YES;
-	}];
+	[NSTimer scheduledTimerWithTimeInterval:1.0 repeats:NO block:^(NSTimer* meow) { [UIApplication sharedApplication].idleTimerDisabled = YES; }];
 }
--- src/main.m
+++ src/main.m
@@ -1,31 +1,30 @@
-#import <spawn.h>
-#import <dlfcn.h>
 #import "utils/utils.h"
+#import <dlfcn.h>
+#import <spawn.h>
 
 extern char** environ;
 int ptrace(int, pid_t, caddr_t, int);
 
-__attribute__((constructor)) static void entry(int argc, char **argv, char *envp[])
-{
-    NSLog(@"mrow INIT");
-    if (argc > 1 && strcmp(argv[1], "--jit") == 0) {
-	    NSLog(@"mrow exiting");
-            ptrace(0, 0, 0, 0);
-            exit(0);
-    } else {
-	    NSLog(@"mrow crac");
-        pid_t pid;
-        char *modified_argv[] = {argv[0], "--jit", NULL };
-        int ret = posix_spawnp(&pid, argv[0], NULL, NULL, modified_argv, envp);
-        if (ret == 0) {
-            waitpid(pid, NULL, WUNTRACED);
-            ptrace(11, pid, 0, 0);
-            kill(pid, SIGTERM);
-            wait(NULL);
-        }
-    }
-    init_bypassDyldLibValidation();
-    init_fixCydiaSubstrate();
+__attribute__((constructor)) static void entry(int argc, char** argv, char* envp[]) {
+	NSLog(@"mrow INIT");
+	if (argc > 1 && strcmp(argv[1], "--jit") == 0) {
+		NSLog(@"mrow exiting");
+		ptrace(0, 0, 0, 0);
+		exit(0);
+	} else {
+		NSLog(@"mrow crac");
+		pid_t pid;
+		char* modified_argv[] = { argv[0], "--jit", NULL };
+		int ret = posix_spawnp(&pid, argv[0], NULL, NULL, modified_argv, envp);
+		if (ret == 0) {
+			waitpid(pid, NULL, WUNTRACED);
+			ptrace(11, pid, 0, 0);
+			kill(pid, SIGTERM);
+			wait(NULL);
+		}
+	}
+	init_bypassDyldLibValidation();
+	init_fixCydiaSubstrate();
 
-    init_loadGeode();
+	init_loadGeode();
 }

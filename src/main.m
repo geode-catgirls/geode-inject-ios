207dcf76e1337f0feb01ee4c8484da793da9061f
Fire <17692105+FireMario211@users.noreply.github.com>
2025-09-26 16:15:52 +0000

    swizzle didFinishLaunchingWithOptions because this might be TOO early

--- src/main.m
+++ src/main.m
@@ -1,9 +1,18 @@
 #import "utils/utils.h"
+#include <objc/runtime.h>
 #import <dlfcn.h>
 #import <spawn.h>
 
 extern char** environ;
 int ptrace(int, pid_t, caddr_t, int);
+
+@implementation NSObject (modif_AppController)
+- (BOOL)mrow_application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions {
+	NSLog(@"mrow call didFinishLaunchingWithOptions!");
+	init_loadGeode();
+	return [self rly_application:application didFinishLaunchingWithOptions:launchOptions];
+}
+@end
 
 __attribute__((constructor)) static void entry(int argc, char** argv, char* envp[]) {
 	NSLog(@"mrow INIT");
@@ -26,5 +35,18 @@ __attribute__((constructor)) static void entry(int argc, char** argv, char* envp
 	init_bypassDyldLibValidation();
 	init_fixCydiaSubstrate();
 
-	init_loadGeode();
+	Class appCtrl = NSClassFromString(@"AppController");
+	if (appCtrl) {
+		{
+			SEL orig = @selector(application:didFinishLaunchingWithOptions:);
+			SEL swizzled = @selector(mrow_application:didFinishLaunchingWithOptions:);
+			Method origMethod = class_getInstanceMethod(appCtrl, orig);
+			Method swzMethod = class_getInstanceMethod([NSObject class], swizzled);
+			if (origMethod && swzMethod) {
+				class_addMethod(appCtrl, swizzled, method_getImplementation(swzMethod), method_getTypeEncoding(swzMethod));
+				method_exchangeImplementations(origMethod, class_getInstanceMethod(appCtrl, swizzled));
+				NSLog(@"mrow swizzle (AppController) application:didFinishLaunchingWithOptions");
+			}
+		}
+	}
 }
